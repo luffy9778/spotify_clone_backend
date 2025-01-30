2148fad2f19d8e2fd23cf1a7c21cdd4f6b62b171
luffy9778 <luffy97780.gmail.com>
2025-01-30 23:33:20 +0530

    testing production

--- config/corsOptions.js
+++ config/corsOptions.js
@@ -1,8 +1,7 @@
 const corsOptions = {
-    // origin: "http://localhost:3000",
-    origin: "react-project-spotify-clone.vercel.app",
-    credentials: true, 
-  };
-  
-  module.exports = corsOptions;
-  
+  origin: "https://react-project-spotify-clone.vercel.app",
+  credentials: true,
+  methods: ["GET", "POST", "PUT", "DELETE", "PATCH"], // Allow these methods
+};
+
+module.exports = corsOptions;
